04e53349757de66a0242b123aefd2a60a26d0797
KONDO Uchio <udzura@udzura.jp>
2013-08-03 14:02:50 +0900

    Initial Commit

---
+++ cocos-akubi/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  cocos-akubi
+//
+//  Created by Uchio on 2013/08/03.
+//  Copyright Uchio 2013年. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+@interface cocos_akubiAppDelegate : NSObject <NSApplicationDelegate>
+{
+	NSWindow	*window_;
+	CCGLView	*glView_;
+}
+
+@property (assign) IBOutlet NSWindow	*window;
+@property (assign) IBOutlet CCGLView	*glView;
+
+- (IBAction)toggleFullScreen:(id)sender;
+
+@end
---
+++ cocos-akubi/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  cocos-akubi
+//
+//  Created by Uchio on 2013/08/03.
+//  Copyright Uchio 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
